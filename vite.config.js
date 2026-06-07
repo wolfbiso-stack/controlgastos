faceadf764ea501d69a44ffc7ca4e314b8edbac4
Carlos Culebro <wolfbiso@gmail.com>
2026-06-07 12:58:39 -0600

    Actualizar build en carpeta docs

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/controlgastos/'
+  base: '/controlgastos/',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true
+  }
 })
